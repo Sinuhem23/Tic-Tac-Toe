2f2c9e8564db40071e178f83bef6034d1d4c0b17
sinuhem23 <sinuhem23@gmail.com>
2021-09-22 23:31:42 -0400

    game works

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -4,7 +4,7 @@ import Square from './Square';
 const Board = ({ squares, onClick }) => (
   <div className='board'>
     {squares.map((square, i) => (
-      <Square key={i} value={square} onClick={() => onclick(i)} />
+      <Square key={i} value={square} onClick={() => onClick(i)} />
     ))}
   </div>
 );
--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Game from './Components/Game';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<Game />, document.getElementById('root'));
